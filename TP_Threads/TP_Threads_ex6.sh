6d3ddd461147ec9ccb6280d0109fb354029aafd9
Julien Dehos <dehos@univ-littoral.fr>
2018-03-28 08:53:31 +0200

    gnuplot en png

--- TP_Threads/TP_Threads_ex6.sh
+++ TP_Threads/TP_Threads_ex6.sh
@@ -11,6 +11,6 @@ echo "calcul avec 4 threads..."
 
 echo "generation et affichage des resultats..."
 #TODO faire un paste des fichiers de temps dans un fichier graph.dat
-gnuplot -e "set out 'graph_time.eps'; set terminal postscript; set style data linespoints; plot 'graph.dat' using 1:3, 'graph.dat' using 1:6, 'graph.dat' using 1:9"
-gnuplot -e "set out 'graph_speedup.eps'; set terminal postscript; set style data linespoints; plot 'graph.dat' using 1:(\$3/\$9), 'graph.dat' using 1:(\$3/\$6)"
+gnuplot -e "set out 'graph_time.png'; set terminal png; set style data linespoints; plot 'graph.dat' using 1:3, 'graph.dat' using 1:6, 'graph.dat' using 1:9"
+gnuplot -e "set out 'graph_speedup.png'; set terminal png; set style data linespoints; plot 'graph.dat' using 1:(\$3/\$9), 'graph.dat' using 1:(\$3/\$6)"
 
